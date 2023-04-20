ca0fd409df5bf503e961972a7bfea03f4f41384b
Hann0T <hansolivas1@gmail.com>
2023-04-20 13:08:22 -0500

    refactor: using vue-router.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import Home from './pages/Home.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: Home, name: 'Home' },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
